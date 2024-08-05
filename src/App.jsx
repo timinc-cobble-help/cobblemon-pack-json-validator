c0e06d592fb6a8325cb93393703dbace3da5183c
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-08-04 17:59:39 -0700

    added dl for fixed version

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useCallback, useState } from "react";
 import Page from "./components/structure/Page";
 import "./style.scss";
 import { useEffect } from "react";
-import { validateJsonsInZip } from "./util";
+import { repairJsonsInZip, validateJsonsInZip } from "./util";
+import downloadFile from "downloadfile-js";
 
 function App() {
   const [file, setFile] = useState(null);
@@ -14,47 +15,81 @@ function App() {
   }, []);
 
   useEffect(() => {
-    let running = true
-    validateJsonsInZip(file)
-      .then((res) => {
-        if (!running) return
-        setResults(res?.sort((a, b) => a.valid - b.valid))
-      })
+    let running = true;
+    validateJsonsInZip(file).then((newResults) => {
+      if (!running) return;
+      setResults(newResults?.sort((a, b) => a.valid - b.valid));
+    });
     return () => {
-      running = false
-    }
-  }, [file])
+      running = false;
+    };
+  }, [file]);
 
   const handleReset = useCallback(() => {
     setFile(null);
     setResults(null);
-  }, [])
+  }, []);
 
-  return <Page name="Cobblemon Pack JSON Validator">
-    {
-      !results
-        ? <>
+  const handleDownload = useCallback(async () => {
+    downloadFile(await repairJsonsInZip(file), file.name);
+  }, [file]);
+
+  return (
+    <Page name="Cobblemon Pack JSON Validator">
+      {!results ? (
+        <>
           <h2>Gimme yer pack</h2>
-          <p>Having troubles with your setup? Get told that an addon you&apos;ve installed is the most likely culprit? Run the addons you&apos;ve added to your modpack in here and it&apos;ll tell you if it&apos;s got an invalid JSON file inside of it.</p>
-          <p>Don&apos;t worry, it doesn&apos;t actually upload the file to some server somewhere, it just reviews it using your browser.</p>
+          <p>
+            Having troubles with your setup? Get told that an addon you&apos;ve
+            installed is the most likely culprit? Run the addons you&apos;ve
+            added to your modpack in here and it&apos;ll tell you if it&apos;s
+            got an invalid JSON file inside of it.
+          </p>
+          <p>
+            Don&apos;t worry, it doesn&apos;t actually upload the file to some
+            server somewhere, it just reviews it using your browser.
+          </p>
           <input type="file" accept=".zip" onChange={handleFileChange} />
         </>
-        : <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
-          <div style={{ display: "flex", justifyContent: "space-between", paddingBottom: "1rem" }}>
+      ) : (
+        <div
+          style={{ height: "100%", display: "flex", flexDirection: "column" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              paddingBottom: "1rem",
+            }}
+          >
             <h2>Here&apos;s your results</h2>
-            <button onClick={handleReset}>Reset</button>
+            <div style={{ display: "flex", gap: "0.5em" }}>
+              <button onClick={handleDownload}>Download</button>
+              <button className="secondary" onClick={handleReset}>
+                Reset
+              </button>
+            </div>
           </div>
-          <div className="overflow-auto" style={{ minHeight: 0, flexBasis: 0, flexGrow: 1 }}>
+          <div
+            className="overflow-auto"
+            style={{ minHeight: 0, flexBasis: 0, flexGrow: 1 }}
+          >
             {results.map((result) => (
               <details key={result.filename}>
-                <summary role="button" className={result.valid ? "secondary" : "primary"}>{result.filename}</summary>
+                <summary
+                  role="button"
+                  className={result.valid ? "secondary" : "primary"}
+                >
+                  {result.filename}
+                </summary>
                 <p>{result.error || "No issues here :)"}</p>
               </details>
             ))}
           </div>
         </div>
-    }
-  </Page>;
+      )}
+    </Page>
+  );
 }
 
 export default App;
--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
 import JSZip from 'jszip';
-import validate from "./jsonutil"
+import validate from "./jsonutil";
+import { jsonrepair } from "jsonrepair";
 
 const validateJsonsInZip = async (file) => {
     if (!file) return Promise.resolve(null);
@@ -33,7 +34,35 @@ const validateJsonsInZip = async (file) => {
         };
 
         reader.readAsArrayBuffer(file);
-    })
+    });
 };
 
-export { validateJsonsInZip }
+const repairJsonsInZip = async (file) => {
+    if (!file) return Promise.resolve(null);
+
+    return new Promise((resolve) => {
+        const zip = new JSZip();
+        const reader = new FileReader();
+
+        reader.onload = async (e) => {
+            const content = e.target.result;
+            const loadedZip = await zip.loadAsync(content);
+            const newFile = new JSZip();
+
+            for (const filename of Object.keys(loadedZip.files)) {
+                try {
+                    const fileContent = await loadedZip.file(filename).async('string');
+                    newFile.file(filename, filename.endsWith(".json") ? jsonrepair(fileContent) : fileContent);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
+            resolve(newFile.generateAsync({ type: "blob" }));
+        };
+
+        reader.readAsArrayBuffer(file);
+    });
+};
+
+export { validateJsonsInZip, repairJsonsInZip };
